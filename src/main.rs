8c37313193c54426c68cd65683dbde63dc2b0d56
frontendbegin <nocturnalmichael5@gmail,com>
2024-08-14 13:13:52 +0100

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#[allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    // let x: i32 = 5;
+    // let _y: i32 = 4;
+
+    // assert_eq!(x,5);
+    // println!("Success!")
+    // let mut x = 1;
+    // x += 2;
+    // assert_eq!(x, 3);
+    // println!("Success!");
+    // let x = 10;
+    //     let y = 5;
+    // {
+        
+    //     println!("The value of x is {} and the value of y is {}", x, y)
+    // }
+    // println!("The value of x is {} and the value of y is {}", x, y)
+    // define_x();
+    let i: i32 = 3;
 }
+
+// fn define_x()
+// {
+//     let x = "hello";
+//     println!("{}, world", x);
+// }
