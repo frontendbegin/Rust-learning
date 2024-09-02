cbb7532435d5cb5ab69d6ea9fe51e9c062b8069c
frontendbegin <nocturnalmichael5@gmail,com>
2024-09-02 13:32:57 +0100

    tried to merge repo failed

---
+++ enums/target/debug/deps/enums-264cff6bd18692a9.d
@@ -0,0 +1,5 @@
+C:\Users\EMI\OneDrive\Documents\Learning rust\volume 6\enums\target\debug\deps\libenums-264cff6bd18692a9.rmeta: src/main.rs
+
+C:\Users\EMI\OneDrive\Documents\Learning rust\volume 6\enums\target\debug\deps\enums-264cff6bd18692a9.d: src/main.rs
+
+src/main.rs:
---
+++ enums/target/debug/deps/enums-63a96a3ec44d4174.d
@@ -0,0 +1,5 @@
+C:\Users\EMI\OneDrive\Documents\Learning rust\volume 6\enums\target\debug\deps\libenums-63a96a3ec44d4174.rmeta: src/main.rs
+
+C:\Users\EMI\OneDrive\Documents\Learning rust\volume 6\enums\target\debug\deps\enums-63a96a3ec44d4174.d: src/main.rs
+
+src/main.rs:
